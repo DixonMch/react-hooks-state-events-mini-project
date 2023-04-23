01ab347e5724d3336771a53293cc4d94dc30fe8a
DixonMch <dixonmogaka@gmail.com>
2023-04-23 23:05:03 +0300

    Completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { CATEGORIES } from "../data";
 
 function CategoryFilter() {
+  const handleClick = (category) => {
+    // handle button click here
+  };
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {CATEGORIES.map((category) => (
+        <button key={category} onClick={() => handleClick(category)}>
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
